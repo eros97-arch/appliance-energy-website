4aba0c2d9b39ad8bdcf9b798470797e4d658cff7
eros97-arch <awgsyahrulakmal@gmail.com>
2026-09-12 20:00:55 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const navLinks = document.querySelectorAll(".nav-links a");
+
+navLinks.forEach(function(link) {
+
+    link.addEventListener("click", function() {
+
+        navLinks.forEach(function(item) {
+            item.classList.remove("active");
+        });
+
+        this.classList.add("active");
+
+    });
+
+});
